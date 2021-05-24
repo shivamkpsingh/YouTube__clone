77a3bfb7aecea214645a5e77e6214a3f1c213f98
shivam12372 <shivamkpsingh@gmail.com>
2021-05-24 20:31:44 +0530

    initial commit

---
+++ src/Recommendedvideos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Recommendedvideos.css'
+
+const Recommendedvideos=()=> {
+    return (
+        <div className="recommendedvideos">
+            <h2>here is video is going</h2>
+        </div>
+    )
+}
+
+export default Recommendedvideos
---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import SidebarRow from './SidebarRow'
+import './Sidebar.css'
+import HomeIcon from '@material-ui/icons/Home';
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <SidebarRow selected Icon={HomeIcon}  title="Home" />
+            <SidebarRow Icon={WhatshotIcon} title="Trending" />
+            <SidebarRow  Icon={SubscriptionsIcon} title="Subscription" />
+            <hr />
+        </div>
+    )
+}
+
+export default Sidebar
