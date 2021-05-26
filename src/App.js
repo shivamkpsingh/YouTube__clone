a36b243260f4d55aa7324da38a1a524c88485439
shivam12372 <shivamkpsingh@gmail.com>
2021-05-26 21:11:24 +0530

    add new feature

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import './App.css'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Recommendedvideos from './Recommendedvideos'
-import {BrowserRouter as Router,Switch ,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-    <div className="app">
-      <Header />
-     <div className="app__page">
-       <Sidebar/>
-       <Recommendedvideos/>
-     </div>
-    </div>
-      
+      <div className="app">
+
+        <Router>
+        <Header />
+
+          <Switch>
+            <Route path="/search">
+              <h1>this is search page</h1>
+            </Route>
+            <Route path="/">
+              <div className="app__page">
+                <Sidebar />
+                <Recommendedvideos />
+              </div>
+            </Route>
+          </Switch>
+        </Router>
+
+
+
+
+
+      </div>
+
     </>
   )
 
